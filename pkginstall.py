f77185d45d91c676c7141d2f79de4aabe3ce0dae
SBCT-TEST <109532158+SBCT-TEST@users.noreply.github.com>
2022-07-18 20:10:39 +0600

    Add files via upload

---
+++ pkginstall.py
@@ -0,0 +1,26 @@
+import os
+import time
+os.system("clear")
+print("""
+
+
+  /$$$$$$  /$$$$$$$   /$$$$$$  /$$$$$$$$
+ /$$__  $$| $$__  $$ /$$__  $$|__  $$__/
+| $$  \__/| $$  \ $$| $$  \__/   | $$   
+|  $$$$$$ | $$$$$$$ | $$         | $$   
+ \____  $$| $$__  $$| $$         | $$   
+ /$$  \ $$| $$  \ $$| $$    $$   | $$   
+|  $$$$$$/| $$$$$$$/|  $$$$$$/   | $$   
+ \______/ |_______/  \______/    |__/   
+                                        
+          coder: svjs
+          facebooknsksk                                                         
+""")
+time.sleep(3)
+os.system("clear")
+os.system("pkg install sl")
+os.system("pkg install git -y")
+print("")
+print("")
+print("")
+print("all package installed succesfully")
